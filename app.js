14b54e46f9bc11d22d5db4694b17b508c1907872
unknown <jreidmke@gmail.com>
2020-11-11 11:37:13 -0600

    added randNum component to app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const Hello = () => {
-    return <h1>Hello</h1>;
+    return <h1>Hello</h1>
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('root'));
+const Goodbye = () => {
+    return <h2>Goodbye</h2>
+}
+
+const App = () => {
+    return (
+        <div>
+            <Hello/>
+            <RandomNum/><RandomNum/>
+            <Goodbye/>
+            <Hello/>
+            <Hello/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
